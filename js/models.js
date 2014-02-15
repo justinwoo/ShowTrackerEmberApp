ce11b35e9b50ef94e9fbc63742547e14c9a5ca73
Justin Woo <moomoowoo@gmail.com>
2014-02-15 16:03:06 -0600

    moved navbar to a partial, added more fixtures

--- js/models.js
+++ js/models.js
@@ -1,16 +1,22 @@
 App.Show = DS.Model.extend({
-  title: DS.attr('string'),
-  episode: DS.attr('number')
+    title: DS.attr('string'),
+    episode: DS.attr('number')
 });
 
-App.Show.FIXTURES = [
-{
-  id: '1',
-  title: '俺のお嫁さんと一心同体',
-  episode: '1'
-}, 
-{
-  id: '2',
-  title: '俺のお嫁さんと一心同体2',
-  episode: '3'
+App.Show.FIXTURES = [{
+    id: '1',
+    title: '俺のお嫁さんと一心同体',
+    episode: '1'
+}, {
+    id: '2',
+    title: '俺のお嫁さんと一心同体2',
+    episode: '3'
+}, {
+    episode: '7',
+    title: "世界征服 (正解制服)",
+    id: '4'
+}, {
+    episode: '5',
+    title: "いなり、くんくん",
+    id: '11'
 }];
