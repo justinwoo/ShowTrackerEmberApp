91aba2a34a8aefbd8bd1841da6fa0b65c9d06c62
Justin Woo <moomoowoo@gmail.com>
2014-03-04 11:07:00 -0600

    Cleaned up code, englishified text, added a button to the insert show row

--- js/models.js
+++ js/models.js
@@ -3,24 +3,24 @@ App.Show = DS.Model.extend({
     episode: DS.attr('number')
 });
 
-App.ApplicationAdapter = DS.RESTAdapter.extend({
-    namespace: 'service'
-});
+// App.ApplicationAdapter = DS.RESTAdapter.extend({
+//     namespace: 'service'
+// });
 
-App.Show.FIXTURES = [{
-    id: '1',
-    title: '俺のお嫁さんと一心同体',
-    episode: '1'
-}, {
-    id: '2',
-    title: '俺のお嫁さんと一心同体2',
-    episode: '3'
-}, {
-    episode: '7',
-    title: "世界征服 (正解制服)",
-    id: '4'
-}, {
-    episode: '5',
-    title: "いなり、くんくん",
-    id: '11'
-}];
+// App.Show.FIXTURES = [{
+//     id: '1',
+//     title: '俺のお嫁さんと一心同体',
+//     episode: '1'
+// }, {
+//     id: '2',
+//     title: '俺のお嫁さんと一心同体2',
+//     episode: '3'
+// }, {
+//     episode: '7',
+//     title: "世界征服 (正解制服)",
+//     id: '3'
+// }, {
+//     episode: '5',
+//     title: "いなり、くんくん",
+//     id: '4'
+// }];
